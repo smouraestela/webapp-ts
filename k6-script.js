9d7039fba3f05bed150cb6a21f5dc3dbc830f3cd
Estela Moura <smoura.estela@gmail.com>
2025-12-05 14:53:21 -0300

    Adiciona thresholds para o k6

--- k6-script.js
+++ k6-script.js
@@ -4,6 +4,15 @@ import { check, sleep } from 'k6';
 export const options = {
   vus: 5,              // 5 usuários virtuais
   duration: '30s',     // teste de 30 segundos
+  thresholds: {
+    // 95% das requisições com p(95) < 200ms
+    http_req_duration: ['p(95)<200'],
+    // menos de 1% das requisições podem falhar
+    http_req_failed: ['rate<0.01'],
+    // opcional: todas as checks com 100% de sucesso
+    checks: ['rate>0.99'],
+  }
+
 };
 
 export default function () {
